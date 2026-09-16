9f8689af69bb452c66526313292e668cde6e8115
FridaEngstroem <fren8988@student.uu.se>
2026-09-16 15:52:22 +0200

    new
    open the files and installed seaborn as well as adding the bt file that was missing

--- cleandata_projekt.py
+++ cleandata_projekt.py
@@ -1,11 +1,17 @@
+import csv
+
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('Geeksforgeeks/Data/diabetes.csv')
-df.head()
+gender= pd.read_csv("genders.csv")
+fbfriends= pd.read_csv('fb_friends.csv')
+calls= pd.read_csv('calls.csv')
+sms= pd.read_csv('sms.csv')
+bt= pd.read_csv('bt_symmetric.csv')
 
-#hej gemma 
+
+
 
