7385502e4ad46d63d6549e9ee072a1cd051e11b0
COEN5007 <cornelia.engman.5007@student.uu.se>
2026-09-18 14:04:44 +0200

    new

--- cleandata_corre.py
+++ cleandata_corre.py
@@ -1,4 +1,19 @@
+#cleaning sms conversations 
+
 import pandas as pd #python library for working with data, can give us answers about the data we're analysing 
-duplicatescleaned_df = df.drop_duplicates()
+import numpy as np 
+import seaborn as sns 
+
+# duplicatescleaned_df = df.drop_duplicates() # vet ej om denna behövs 
+sms = pd.read_csv("sms.csv")
+
+print (sms)
+
+duplicatescleaned_sms = sms.drop_duplicates() #tar bort duplicates 
+
+print (duplicatescleaned_sms)
+
+sms.cleaned = sns.load_dataset("duplicatescleaned_sms")
+
 
 
